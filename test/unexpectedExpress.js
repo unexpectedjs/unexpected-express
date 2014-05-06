aa97b0c4871293ced4d54ab347486f4cba4514ae
Andreas Lind Petersen <andreas@one.com>
2014-05-06 10:01:53 +0200

    Default to GET when no method is specified.
    Started a test suite.

---
+++ test/unexpectedExpress.js
@@ -0,0 +1,110 @@
+var unexpectedExpress = require('../lib/unexpectedExpress'),
+    unexpected = require('unexpected'),
+    bodyParser = require('body-parser'),
+    BufferedStream = require('bufferedstream'),
+    express = require('express');
+
+describe('unexpectedExpress', function () {
+    var expect = unexpected.clone().installPlugin(unexpectedExpress);
+
+    it('should default to GET when no method is provided', function (done) {
+        expect(express().use(function (req, res, next) {
+            expect(req.method, 'to equal', 'GET');
+            next();
+        }), 'to be middleware that processes', {response: 404}, done);
+    });
+
+    it('supports the request body to be specified as a string', function (done) {
+        expect(express().use(bodyParser.urlencoded()).use(function (req, res, next) {
+            res.send('Hello ' + req.param('foo') + ' and ' + req.param('baz'));
+        }), 'to be middleware that processes', {
+            request: {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: 'foo=bar&baz=quux'
+            },
+            response: {
+                statusCode: 200,
+                body: 'Hello bar and quux'
+            }
+        }, done);
+    });
+
+    it('supports the request body to be specified as a Buffer', function (done) {
+        expect(express().use(bodyParser.urlencoded()).use(function (req, res, next) {
+            res.send('Hello ' + req.param('foo') + ' and ' + req.param('baz'));
+        }), 'to be middleware that processes', {
+            request: {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: new Buffer('foo=bar&baz=quux', 'utf-8')
+            },
+            response: {
+                statusCode: 200,
+                body: 'Hello bar and quux'
+            }
+        }, done);
+    });
+
+    it('supports the request body to be specified as a stream that emits strings', function (done) {
+        var requestBodyStream = new BufferedStream();
+        setImmediate(function () {
+            requestBodyStream.end('foo=bar&baz=quux');
+        });
+        expect(express().use(bodyParser.urlencoded()).use(function (req, res, next) {
+            res.send('Hello ' + req.param('foo') + ' and ' + req.param('baz'));
+        }), 'to be middleware that processes', {
+            request: {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: requestBodyStream
+            },
+            response: {
+                statusCode: 200,
+                body: 'Hello bar and quux'
+            }
+        }, done);
+    });
+
+    it('supports the request body to be specified as a stream that emits Buffers', function (done) {
+        var requestBodyStream = new BufferedStream();
+        expect(express().use(bodyParser.urlencoded()).use(function (req, res, next) {
+            res.send('Hello ' + req.param('foo') + ' and ' + req.param('baz'));
+        }), 'to be middleware that processes', {
+            request: {
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                body: requestBodyStream
+            },
+            response: {
+                statusCode: 200,
+                body: 'Hello bar and quux'
+            }
+        }, done);
+        setImmediate(function () {
+            requestBodyStream.end(new Buffer('foo=bar&baz=quux', 'utf-8'));
+        });
+    });
+
+    it('supports the request body to be specified as an object (JSON)', function (done) {
+        var requestBodyStream = new BufferedStream();
+        expect(express().use(bodyParser.json()).use(function (req, res, next) {
+            res.send('Hello ' + req.param('foo') + ' and ' + req.param('baz'));
+        }), 'to be middleware that processes', {
+            request: {
+                body: {foo: 'bar', baz: 'quux'}
+            },
+            response: {
+                statusCode: 200,
+                body: 'Hello bar and quux'
+            }
+        }, done);
+        setImmediate(function () {
+            requestBodyStream.end(new Buffer('foo=bar&baz=quux', 'utf-8'));
+        });
+    });
+});
