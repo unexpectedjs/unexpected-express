ee0ddbb2b9fb160d178393d239ccf80892333b5f
Alex J Burke <alex@alexjeffburke.com>
2018-10-07 12:16:14 +0200

    Issue a clone() in the unexpected-markdown bootstrap script.
    This is in preparation for Unexpected v11 which is frozen at the
    top level. The change is fully backward compatible.

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -1,6 +1,6 @@
 /* eslint no-unused-vars: "off" */
 /*global unexpected:true, express:true*/
-unexpected = require('unexpected');
+unexpected = require('unexpected').clone();
 express = require('express');
 unexpected.output.preferredWidth = 80;
 unexpected.installPlugin(require('./lib/unexpectedExpress'));
