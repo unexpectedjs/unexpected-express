7eab2471fecc208d745b997c1f3b8bfd4648e41b
Andreas Lind <andreaslindpetersen@gmail.com>
2020-11-22 11:01:01 +0100

    Don't use sinon.useFakeTimers

--- bootstrap-unexpected-markdown.js
+++ bootstrap-unexpected-markdown.js
@@ -4,8 +4,3 @@ unexpected = require('unexpected').clone();
 express = require('express');
 unexpected.output.preferredWidth = 80;
 unexpected.installPlugin(require('./lib/unexpectedExpress'));
-
-require('sinon').useFakeTimers({
-  now: 1457823364125,
-  toFake: ['Date'],
-});
